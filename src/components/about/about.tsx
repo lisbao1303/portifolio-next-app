b825e8dc8047ad6108c5ccc665afa3968a71885b
lisbao1303 <gabriel.lisbao@gmail.com>
2025-03-11 12:40:50 -0300

    api interna

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -1,7 +1,7 @@
-
+"use client"
 import CardTimeLine from "./cardtimeline/cardtimeline";
 import styles from "./about.module.css";
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { userPublicDataService } from '../../services/userPublicDataService';
 import { UserPublicData } from '../../models/userPublicData';
 import SideAbout from "../sideabout/sideabout";
@@ -27,33 +27,47 @@ const initializeUserPublicData = (): UserPublicData => ({
     additionalInfo: ""
 });
 
-export default async function About() {
+const About = () => {
+    // State para armazenar os dados carregados
+    const [userPublicData, setUserPublicData] = useState<UserPublicData>(initializeUserPublicData());
+    const [experience, setExperience] = useState<Experience[]>([]);
+    const [education, setEducation] = useState<Education[]>([]);
 
-    const userPublicData: UserPublicData[] = await userPublicDataService.getAll();
-    const experience: Experience[] = await experienceService.getAll();
-    const education: Education[] = await educationService.getAll();
+    // Carregar os dados em tempo de execução
+    useEffect(() => {
+        // Função para buscar os dados da API
+        const loadData = async () => {
+            const userData = await userPublicDataService.getAll();
+            const experienceData = await experienceService.getAll();
+            const educationData = await educationService.getAll();
+
+            setUserPublicData(userData[0]); // Assume que você sempre tem pelo menos 1 dado
+            setExperience(experienceData);
+            setEducation(educationData);
+        };
+
+        loadData();
+    }, []); // O array vazio faz o efeito rodar uma vez na montagem do componente
 
     return (
         <div id="cvAbout" className={styles.aboutContainer}>
-
-            <SideAbout publicData={userPublicData[0]} />
-
+            <SideAbout publicData={userPublicData} />
             <div className={styles.fullscreen}>
                 <div className={styles.card}>
                     <h2 className={styles.card_title}>About me.</h2>
-                    <p className={styles.card_content} dangerouslySetInnerHTML={{ __html: userPublicData[0].about }}></p>
+                    <p className={styles.card_content} dangerouslySetInnerHTML={{ __html: userPublicData.about }}></p>
                 </div>
                 <p className={"destak " + styles.wordsize}> Education</p>
                 {education.map((item, index) => (
                     <CardTimeLineEdu key={index} education={item} />
                 ))}
                 <p className={"destak " + styles.wordsize}> Experience</p>
-
                 {experience.map((item, index) => (
                     <CardTimeLine key={index} experience={item} />
                 ))}
             </div>
         </div>
-
     );
-}
+};
+
+export default About;
